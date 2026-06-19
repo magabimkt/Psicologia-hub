8f5630b981d90ef84c7e430a93197174b5739d98
magabimkt <magabi.mkt@gmail.com>
2026-06-19 10:28:23 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,25 @@
-const CACHE = 'psi2-v1';
-const ASSETS = [
-  './',
-  './index.html',
+const CACHE = 'psihub-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png',
   'https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght@0,9..144,300;0,9..144,600;0,9..144,700;1,9..144,300&family=Atkinson+Hyperlegible:wght@400;700&family=IBM+Plex+Mono:wght@400;500&display=swap'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
+  // Don't intercept cross-origin iframe requests
+  if (new URL(e.request.url).origin !== location.origin) {
+    return;
+  }
   e.respondWith(
     caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
       const clone = res.clone();
